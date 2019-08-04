ee935a0d80441da529b3230edd5559ceccc0cf95
Dmitri Stasenko <StaDimVyach@mail.ru>
2019-08-04 21:54:15 +0300

    Remove unnecessary code

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,24 +1,3 @@
-// var requestURL = 'http://localhost:3000/catalog.json';
-// var request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.responseType = 'json';
-// request.send();
-
-// request.onload = function() {
-//     var myJSON = request.response;
-
-//     myJSON.lamps.forEach(item => {
-//         $('.catalog__items').append(`
-//             <div class="catalog__item">
-//                 <div class="catalog__item-inner">
-//                     ${item.title} <br>
-//                     ${item.author}
-//                 </div>
-//             </div>
-//         `)
-//     });
-// }
-
 const $filter = $('.catalog__filter');
 const $filterClose = $filter.find('i');
 const $filterOpen = $('.catalog__filter-open');
