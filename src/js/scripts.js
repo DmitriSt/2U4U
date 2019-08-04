42fbac623f16de6b8d9db1a7f82a0bb90b1da613
Dmitri Stasenko <StaDimVyach@mail.ru>
2019-08-04 20:33:48 +0300

    Add logic for close filter

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,9 +10,26 @@ request.onload = function() {
     myJSON.lamps.forEach(item => {
         $('.catalog__items').append(`
             <div class="catalog__item">
-                <div class="catalog__item-inner">${item.title}</div>
-                <div class="catalog__item-inner">${item.author}</div>
+                <div class="catalog__item-inner">
+                    ${item.title} <br>
+                    ${item.author}
+                </div>
             </div>
         `)
     });
 }
+
+const $filter = $('.catalog__filter');
+const $filterClose = $filter.find('i');
+const $filterOpen = $('.catalog__filter-open');
+const $catalogItems = $('.catalog__items');
+$filterClose.click(function() {
+    $filter.addClass('catalog__filter_closed');
+    $filterOpen.addClass('catalog__filter-open_show');
+    $catalogItems.addClass('catalog__items_full-width');
+});
+$filterOpen.click(function() {
+    $filter.removeClass('catalog__filter_closed');
+    $filterOpen.removeClass('catalog__filter-open_show');
+    $catalogItems.removeClass('catalog__items_full-width');
+});
